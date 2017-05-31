c916a5b8d72ab43946ac58b69a038697394f35a0
Jordan Hudgens <jordan@wow.com>
2017-05-31 15:58:09 -0700

    cleaned up controller

--- app/controllers/hand_raises_controller.rb
+++ app/controllers/hand_raises_controller.rb
@@ -1,19 +1,16 @@
 class HandRaisesController < ApplicationController
   before_action :set_hand_raise, only: [:show, :update, :destroy]
 
-  # GET /hand_raises
   def index
     @hand_raises = HandRaise.all
 
     render json: @hand_raises
   end
 
-  # GET /hand_raises/1
   def show
     render json: @hand_raise
   end
 
-  # POST /hand_raises
   def create
     @hand_raise = HandRaise.new(hand_raise_params)
 
@@ -24,7 +21,6 @@ class HandRaisesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /hand_raises/1
   def update
     if @hand_raise.update(hand_raise_params)
       render json: @hand_raise
@@ -33,18 +29,15 @@ class HandRaisesController < ApplicationController
     end
   end
 
-  # DELETE /hand_raises/1
   def destroy
     @hand_raise.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_hand_raise
       @hand_raise = HandRaise.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def hand_raise_params
       params.require(:hand_raise).permit(:question, :body, :student_email, :student_name, :guide_title, :guide_id, :resolution, :ta_name, :ta_email, :status)
     end
