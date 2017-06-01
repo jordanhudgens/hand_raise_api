4b711ae09a16d3a9bbdf2f90cdc0f8e357c9ef72
Jordan Hudgens <jordan@wow.com>
2017-06-01 13:30:22 -0700

    Added debugger to background job

--- app/jobs/update_devcamp_hand_raise_job.rb
+++ app/jobs/update_devcamp_hand_raise_job.rb
@@ -2,10 +2,8 @@ class UpdateDevcampHandRaiseJob < ApplicationJob
   queue_as :default
 
   def perform(hand_raise)
-    UpdateDevcamp.patch(hand_raise)
-  end
-
-  def perform_two
-    puts "asdfasdfasfdasfasdfasdasdasdfasda"
+    #UpdateDevcamp.patch(hand_raise)
+    puts hand_raise
+    puts "-----------------------------------------"
   end
 end
